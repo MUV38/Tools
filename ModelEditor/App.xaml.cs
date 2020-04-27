9498f0ba074d59bbda0fa6c11b7aa72575c74a70
Toru Miyawaki <asuruntoru0803@yahoo.co.jp>
2020-04-27 12:07:29 +0900

    ModelEditor追加

---
+++ ModelEditor/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+using ModelEditor.Main.Views;
+using ModelEditor.Main.ViewModels;
+
+namespace ModelEditor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// スタートアップ
+        /// </summary>
+        /// <param name="e">イベント引数</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // ビューとビューモデルの生成と関連付け
+            var view = new MainView();
+            var viewModel = new MainViewModel();
+            view.DataContext = viewModel;
+
+            // ビュー表示
+            view.Show();
+        }
+    }
+}
---
+++ ModelEditor/Main/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelEditor.Main.ViewModels
+{
+    /// <summary>
+    /// メインウィンドウのビューモデル
+    /// </summary>
+    class MainViewModel
+    {
+    }
+}
